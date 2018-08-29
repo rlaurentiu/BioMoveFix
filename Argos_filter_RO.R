f618386fb90d5d3d467be64311a43f55c2df9ba3
rlaurentiu <laurentiu.rozylowicz@g.unibuc.ro>
2018-08-29 11:45:28 +0300

    Add files via upload

--- Argos_filter_RO.R
+++ Argos_filter_RO.R
@@ -1,25 +1,43 @@
 # Pregatirea setului de date Argos pentru filtrare
 
-# Declaram directorul in care lucram. Acest script citeste datele din
-# subfolderul *data* si va salva rezultatele in subfolderul *output*,
-# astfel trebuie sa cream doua foldere numite output si data, sau sa
-# modificam scriptul acolo unde este necesar
+# Declaram directorul in care lucram. De exemplu C:/Lucru/Argos
+# Atentie calea spre director trebuie declarata folosint back slash
+# C:/Lucru/Argos si NU C:\Lucru\Argos
+# setwd("C:/Lucru/Argos")
 
-setwd("set your working directory")
+setwd("E:/OneDrive/argos_errors/BioMoveFix-master")
 
-# Activam pachetul R dplyr. Daca nu este disponibil trebuie descarcat cu comanda
-# install.packages(dplyr). Tutorialul pentru acest pachet R este la adresa web
+# Instalam pachetele cu care vom lucra. Aceasta operatie se face doar
+# prima data. Daca rerulati scriptul atunci puteti trece peste aceasta
+# operatie.
+
+install.packages("dplyr")
+install.packages("sp")
+install.packages("mapview")
+install.packages("gdalUtils")
+install.packages("argosfilter")
+install.packages("SDLfilter")
+
+
+# Activam pachetul R dplyr. Tutorialul pentru acest pachet R este la adresa web
 # https://dplyr.tidyverse.org/
 
 library(dplyr)
 
 # Se incarca fisierul cu date brute Argos (export csv din Argos CLS, cu datele de
-# diagnostic). Vom lucra cu fisier date Argos PTT Geotrack 23G SOLAR PTT
-# obtinut in cadrul proiectului PN-III-P2-2.1-PED-2016-056 finantat de UEFISCDI
-# (http://ccmesi.ro/?page_id=47). Setul de date demonstrativ cuprinde
-# 2681 observatii si 39 variabile din zona Saveni, IL (test static)
+# diagnostic). Vom lucra cu date Argos PTT Geotrack 23G SOLAR PTT
+# obtinute in cadrul proiectului PN-III-P2-2.1-PED-2016-056 finantat de UEFISCDI
+# (http://ccmesi.ro/?page_id=47).
+# Setul de date demonstrativ cuprinde 2681 observatii nefiltrate si
+# 39 variabile din zona Saveni, Ialomita (test static)
+# pentru informatii mai amanuntite puteti citi articolul
+# Rozylowicz L., Bodescu, F.P., Ciocanea C.M., Gavrilidis A.A., Manolache S.,
+# Matache M.L., Miu I.V., Moale C., Nita A, Popescu V.D., (2018) Empirical
+# analysis and modelling of Argos Doppler location errors in Romania.
+# bioRxiv 397364; doi:10.1101
+# http://biorxiv.org/cgi/content/short/397364v1
 
-Argos_data <- read.csv(file = "data/argos_data.csv",
+Argos_data <- read.csv(file = "argos_data.csv",
                        header = TRUE,
                        as.is = TRUE)
 
@@ -28,17 +46,20 @@ Argos_data <- read.csv(file = "data/argos_data.csv",
 
 glimpse(Argos_data)
 
-# Filtru date duplicate
+# Filtru pentru eliminarea duplicatelor
 
 # Dupa importul setului de date vom sterge datele duplicate. Date Argos exportate
-# in format csv includ duplicate pentru fiecare localizare, constand in mesaje
+# in format csv includ duplicate pentru fiecare localizare, constand din mesaje
 # multiple pentru fiecare locatie obtinuta de 1 PTT. Cum mesajele pentru o locatie
-# sunt identice cu exceptia marcii temporale Msg.Date vom pastra doar primul mesaj
+# sunt identice cu exceptia marcii temporale (Msg.Date) vom pastra doar primul mesaj
 # din fiecare serie. Consideram duplicate mesajele care au informatie identica
 # pentru variabilele Platform.ID.No. (ID-platformei), Pass.dur...s. (durata
 # receptiei),  Sat. (satelitul care  a generat localizarea), Loc..date (data
 # localizarii). In acest fel excludem posibilitatea sa stergem gresit mesajele.
-# Numele corecte ale variabilelor le identificam usor cu functia glimpse ca mai sus.
+# Numele corecte ale variabilelor le identificam cu functia glimpse ca mai sus.
+# Atentie, daca rulati scriptul cu date proprii introduceti in cod numele variabilelor
+# din setul Dvs. de date. Orice inadvertenta conduce la erori
+# Prin codul de mai jos cream un set de date (argos.dup) fara duplicate
 
 argos.dup <-
   distinct(Argos_data,
@@ -50,11 +71,13 @@ argos.dup <-
 
 # Functia *distinct* a pastrat 521 localizarii din 2681 de mesaje initiale.
 
-# Filtru date fara coordonate si data de localizare
 
-# Din noul set de date *argos.dup* vom  elimina succesiv datele cu NA (lipsa date)
-# in coloanele Loc..date, Longitude, Latitude. Acestea sunt localizari invalide.
-# Utilizam succesiv functia filter reluand setul de date anterior.
+# Filtru eliminare inregistrari fara coordonate si data de localizare
+
+# Din noul set de date *argos.dup* vom  elimina succesiv inregistrarile cu NA
+# (lipsa date) in coloanele Loc..date, Longitude, Latitude. Acestea sunt
+# localizari invalide. Pentru aceasta, Utilizam succesiv functia filter
+# reluand setul de date creat anterior anterior.
 
 argos.dup.1 <-   filter(argos.dup, !is.na(Loc..date))
 argos.dup.2 <-   filter(argos.dup.1, !is.na(Longitude))
@@ -62,15 +85,14 @@ argos.dup.3 <-   filter(argos.dup.2, !is.na(Latitude))
 
 
 # Setul de date final *argos.dup3* reprezinta un set de date prelucrat, gata
-# pentru utilizare in analize.
+# pentru utilizare in analize. S-a eliminat doar o inregistrare, dar in
+# in situatii reale vom avea cu siguranta mai multe cazuri
 
-# Stergem seturile de date intermediare si pastram doar setul final *argos.dup3*
-
-rm("Argos_data", "argos.dup", "argos.dup.1", "argos.dup.2")
 
 # Explorarea datelor
 
-# Vom explora setul de date, de exemplu numaram localizarile per individ (PTT)
+# Putem explora setul de date, pentru a intelege variabilitatea localizarilor,
+# de exemplu numaram localizarile per individ (PTT)
 
 count(argos.dup.3, Platform.ID.No.)
 
@@ -80,7 +102,7 @@ count(argos.dup.3, Platform.ID.No.)
 
 counts <- table(argos.dup.3$Platform.ID.No.)
 
-barplot(counts, main = "Numar de localizari per individ")
+barplot(counts, xlab = "individ monitorizat", ylab = "numar localizari per individ")
 
 
 # Pentru o mai buna intelegere a datelor putem vizualiza localizarile cu ajutorul
@@ -114,13 +136,10 @@ harta_Saveni
 
 # Hartile create au dimensiuni mari, astfel ca pentru reprezentari grafice
 # a unor zone mari recomandam functia plot.
-# Stergem seturile de date intermediare si pastram doar setul final *argos.dup3*
-
-rm("argos_harta", "harta_Saveni", "counts")
 
 # Salvam setul de date prelucrat argos.dup.3 sub numele de argos_clean.csv
 
-write.csv(argos.dup.3, file = "output/argos_clean.csv")
+write.csv(argos.dup.3, file = "argos_clean.csv")
 
 
 # Filtru calitate localizari Argos (Loc..quality).
@@ -138,7 +157,7 @@ argos.dup.3$Loc..quality <- factor(argos.dup.3$Loc..quality,
 
 count_LC <- table(argos.dup.3$Loc..quality)
 
-barplot(count_LC, main = "Numarul de localizari dupa clasa de eroare")
+barplot(count_LC, xlab="Clasa de eroare", ylab="Numar inregistrari")
 
 
 ## Cream un set de date filtrat cu clasele de eroare 3, 2 si 1
@@ -171,7 +190,7 @@ harta2 <-
   )
 harta2
 
-# si le putem vizualizam una langa alta
+# le vizualizam una langa alta pentru comparatie
 
 latticeView(harta1, harta2, sync.cursor = TRUE)
 
@@ -221,14 +240,12 @@ harta1
 # In final salvam versiunea de date filtrate care raspunde cerintelor noastre, in
 # cazul nostrul setul de date cu LC3, LC2 si LC1
 
-write.csv(argos.LC321, file = "output/argos_LC321.csv")
+write.csv(argos.LC321, file = "argos_LC321.csv")
 
 
 # Filtru de viteza
 
-# stergem toate seturile de date si detasam pachetele incarcate pana acum
-
-rm(list = ls())
+# detasam pachetele incarcate pana acum
 
 detach("package:dplyr")
 detach("package:sp")
@@ -236,7 +253,7 @@ detach("package:mapview")
 
 # Incarcam pachetele SDL filter si dplyr, in ordinea aceasta (au functii cu
 # nume asemanatoare care se suprascriu). Daca apar erori la unele functii
-# de mai jos, atunci trebuie sa detasam pachetele (detach) si sa le 
+# de mai jos, atunci trebuie sa detasam pachetele (detach) si sa le
 # reincaram
 
 library(SDLfilter)
@@ -246,7 +263,7 @@ library(dplyr)
 # (argos_clean.cvs). Daca dorim sa lucram cu cele filtrare incarcam fisierul
 # corespunzator
 
-Argos_clean <- read.csv(file = "output/argos_clean.csv",
+Argos_clean <- read.csv(file = "argos_clean.csv",
                         header = TRUE,
                         as.is = TRUE)
 
@@ -268,9 +285,9 @@ argos.renamed <-
 
 glimpse(argos.renamed)
 
-# Transformam variabila cu marca temporala (Datetime1, fost Loc..date) 
+# Transformam variabila cu marca temporala (Datetime1, fost Loc..date)
 # in format data-time POSIXct. Atentie, sistemtul Windows afiseaza campurile
-# cu date/timp in functie de setarile computerului, astfel ca inainte de 
+# cu date/timp in functie de setarile computerului, astfel ca inainte de
 # transformare trebuie sa vedem care este formatul
 
 glimpse(argos.renamed$DateTime1)
@@ -366,25 +383,24 @@ latticeView(harta.brut, harta.speed, sync.cursor = TRUE)
 
 # salvam fisierul cu datele filtrate exclusiv dupa vmax = 18, metoda 1
 
-write.csv(argos.speed4, file = "output/argos.speed4.csv")
+write.csv(argos.speed4, file = "argos.speed4.csv")
 
 # Filtrul dezolvat de Freitas et al. 2008 presupune filtrarea functie de
 # viteza maxima intre doua locatii, vmax = viteaza maxima plauzibila in m/s)
 
 library(argosfilter)
 
-date.filtrate <- vmask(
-  argos.renamed$lat,
-  argos.renamed$lon,
-  argos.renamed$DateTime,
-  vmax = 5)
+date.filtrate <- vmask(argos.renamed$lat,
+                       argos.renamed$lon,
+                       argos.renamed$DateTime,
+                       vmax = 5)
 
 date.filtrate
 
 # atasam infomatia obtinuta setului de date argos.renamed si numaram
 # locatiile filtrate pentru a vedea efectele filtrului
 
-argos.renamed<-
+argos.renamed <-
   mutate(argos.renamed, date.filtrate = date.filtrate)
 
 count(argos.renamed, date.filtrate)
@@ -397,7 +413,7 @@ library(sp)
 argos.filtruv <- argos.renamed
 
 coordinates(argos.filtruv) <- ~ lon + lat
-proj4string(argos.filtruv ) <- "+init=epsg:4326"
+proj4string(argos.filtruv) <- "+init=epsg:4326"
 harta.fitru.viteza <-
   mapview(argos.filtruv , zcol = "date.filtrate", burst = TRUE)
 
@@ -407,13 +423,14 @@ harta.fitru.viteza
 # pastrand doar locatiile marcate ca true si end location
 
 argos_filtrate_5ms <- filter(argos.renamed,
-              date.filtrate == "true" | date.filtrate == "end_location")
+                             date.filtrate == "true" |
+                               date.filtrate == "end_location")
 
 glimpse(argos_filtrate_5ms)
 
-write.csv(argos_filtrate_5m, file = "output/argos.speed.5ms.csv")
+write.csv(argos_filtrate_5ms, file = "argos.speed.5ms.csv")
 
-# Stergem seturile de date din RStudio si detasam pachetele R 
+# Stergem seturile de date din RStudio si detasam pachetele R
 
 rm(list = ls())
 
@@ -431,14 +448,14 @@ detach("package:argosfilter")
 # este un filtru robust pentru conditiile din Romania
 
 # Incarcam datele brute fara duplicate, create anterior (fisier argos_clean.csv).
-# Daca nu le avem create atunci vom curata datele  pana la linia 122 din script 
+# Daca nu le avem create atunci vom curata datele  pana la linia 122 din script
 # (stergerea duplicatelor, stergerea inregistrarilor cu NA pentru marca temporala,
 # longitudine, latitudine)
 
 
-Argos_clean <- read.csv(file = "output/argos_clean.csv",
-                       header = TRUE,
-                       as.is = TRUE)
+Argos_clean <- read.csv(file = "argos_clean.csv",
+                        header = TRUE,
+                        as.is = TRUE)
 
 library(dplyr)
 
@@ -470,11 +487,16 @@ argos.movebank <-
 
 # salvam un fisier txt pentru upload in Movebank. Fisierul il vom incarca in Movebank,
 # Edit studies, Upload data, add Processed Argos data (csv). Inainte de incarcare
-# trebuie sa facem cont si sa activam un studiu...detalii aici ... 
+# trebuie sa facem cont si sa activam un studiu...detalii aici ...
 # https://www.movebank.org/node/11. De asemenea detalii despre cum rulam filtrul
 # gasim aici https://www.movebank.org/node/38 si in articolul open source
 # https://besjournals.onlinelibrary.wiley.com/doi/abs/10.1111/j.2041-210X.2012.00245.x
 
 
-write.table(argos.movebank, file = "output/argos.movebank.txt", sep = "\t",
-            row.names = FALSE, col.names = TRUE)
+write.table(
+  argos.movebank,
+  file = "argos.movebank.txt",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = TRUE
+)
